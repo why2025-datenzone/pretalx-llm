1ae4dc6aa3d84ad8606e1ee5953a0a403de43e04
Erik Tews <why@datenzone.de>
2025-05-09 01:13:30 +0200

    Depend only on the latest stable release of Pretalx

--- pretalx_llm/migrations/0002_llmuserpreference_llmuserpreferenceembedding_and_more.py
+++ pretalx_llm/migrations/0002_llmuserpreference_llmuserpreferenceembedding_and_more.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("event", "0038_eventextralink"),
+        ("event", "0036_alter_event_header_image_alter_event_logo"),
         ("pretalx_llm", "0001_initial"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
